ad34f9ce5656a086a3b22da372c80377e27e93d2
levianas <160236036+levianas@users.noreply.github.com>
2024-08-28 08:27:57 -0300

    j

--- script.js
+++ script.js
@@ -1,67 +1,91 @@
-const caixaPrincipal = document.querySelector(' .caixa-principal');
-const caixaPerguntas = document.querySelector('caixaperguntas')
-const caixaAlternativas = document.querySelector('caixas-alternativas')
-const caixaResultado = document.querySelector('caixa-resultado')
-const textoResultado = document.querySelector('texto-resultado')
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
 
-const alternativa = [
+const alternativas = [
     {
-        enunciado: "No âmbito social, você prefere",
+        enunciado: "No âmbito social, você prefere:",
         alternativa: [
             {
-                texto:"Alternativa 1",
-                afirmação: "Afirmação 1",
+                texto: "Implementar um programa universal de saúde pública com cobertura para todos, porém aumentando os imposots",
+                afirmacao: "Você melhorou a saúde publica, mas os impostos aumentaram em 5x"
             },
             {
-                texto:"Alternativa 2",
-                afirmação: "Afirmação 2",
+                texto: "Introduzir um sistema de renda básica universal que garanta uma quantia mínima de dinheiro a todos os cidadãos",
+                afirmacao: "Você desestimulou o trabalho e gerou preocupações fiscais a longo prazo"
             }
         ]
+
     },
     {
-        enunciado: "No âmbito ambiental, você prefere",
+        enunciado: "No âmbito ambiental, você prefere:",
         alternativa: [
             {
-                texto:"Alternativa 3",
-                afirmação: "Afirmação 3",
+                texto: ": Adotar uma política de proteção rígida de áreas naturais, preservando grandes áreas de florestas e parques nacionais",
+                afirmacao: "Você limitou o desenvolvimento econômico e uso de recursos naturais"
             },
             {
-                texto:"Alternativa 4",
-                afirmação: "Afirmação 4",
+                texto: "Investir em uma vasta rede de transporte público elétrico e infraestrutura para bicicletas",
+                afirmacao: "Você mudou o hábito de muitos e gerou muitos custos para iniciar o projeto"
             }
         ]
+
     },
     {
-        enunciado: "No âmbito tecnológico, você prefere",
+        enunciado: "No âmbito tecnológico, você prefere:",
         alternativa: [
             {
-                texto:"Alternativa 5",
-                afirmação: "Afirmação 5",
+                texto: "Adotar um sistema global de inteligência artificial (IA) para otimizar a gestão de recursos naturais e prever desastres ambientais",
+                afirmacao: "Você gerou panico global sobre a segurança dos dados de todos"
             },
             {
-                texto:"Alternativa 6",
-                afirmação: "Afirmação 6",
+                texto: " Implementar uma política de automação em larga escala para indústrias e serviços, melhorando a eficiência e reduzindo os custos operacionais",
+                afirmacao: "Você gerou grande desemprego e aumentou a desigualdade econômica"
             }
         ]
-    }
+
+    },
 ]
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
-function mostrapergunta (){
-    perguntaAtual = alternativas[atual]
+function mostraPerguntas() {
+    if(atual>= alternativas.length){
+        mostraResultado();
+        return;
+
+    }
+    perguntaAtual = alternativas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
-    mostraAlternativas
+    mostraAlternativas();
 }
 
-function mostraAlternativas (){
+
+function mostraAlternativas() {
     for (const opção of perguntaAtual.alternativa) {
         const botaoAlternativa = document.createElement('button');
-    botaoAlternativa.textContent = opçao.texto;
-    caixaAlternativas.appendChild(botaoAlternativa);
+        botaoAlternativa.textContent = opção.texto;
+        botaoAlternativa.addEventListener("click",  ()=> respostaSelecionada(opção));
+        caixaAlternativas.appendChild(botaoAlternativa);
+
     }
 }
 
-mostrapergunta();
+function respostaSelecionada(opcao){
+    const afirmacoes = opcao.afirmacao;
+    historiaFinal += afirmacoes+" ";
+    atual++;
+mostraPerguntas();
+}
+  
+function mostraResultado (){
+    caixaPerguntas.textContent = "em resumo vc escolheu...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = ""
+}
+    mostraPerguntas();
